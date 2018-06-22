0e1c6f6c2a6ab833e10b5fde3e478051ee63998c
alice-0-kim <aliceykim0828@gmail.com>
2018-06-22 12:30:01 -0700

    cleanup

--- typeahead.js
+++ typeahead.js
@@ -8,7 +8,7 @@ var demo = new Vue({
 
         articles: [
 	{
-		"term": "Apple",
+		"term": "apple",
 		"definition": "a hard round fruit that is white inside and has a smooth green, yellow, or red skin, which is called peel when it has been removed.",
 		"data_community": "UBC IT",
 		"data_steward": "Alice Kim",
@@ -22,8 +22,8 @@ var demo = new Vue({
 		"image": "dog.jpg"
 	},
 	{
-		"term": "Banana",
-		"definition": "a hard round fruit that is white inside and has a smooth green, yellow, or red skin, which is called peel when it has been removed.",
+		"term": "dictionary",
+		"definition": "a book or electronic resource that lists the words of a language (typically in alphabetical order) and gives their meaning, or gives the equivalent words in a different language, often also providing information about pronunciation, origin, and usage.",
 		"data_community": "UBC IT",
 		"data_steward": "Alice Kim",
 		"definition_source": "https://www.google.ca/search?q=apple&oq=apple&aqs=chrome..69i57j69i60l3j69i65l2.1158j1j7&sourceid=chrome&ie=UTF-8",
@@ -36,8 +36,8 @@ var demo = new Vue({
 		"image": "dog.jpg"
 	},
 	{
-		"term": "Cucumber",
-		"definition": "a hard round fruit that is white inside and has a smooth green, yellow, or red skin, which is called peel when it has been removed.",
+		"term": "bliss",
+		"definition": "perfect happiness; great joy.",
 		"data_community": "UBC IT",
 		"data_steward": "Alice Kim",
 		"definition_source": "https://www.google.ca/search?q=apple&oq=apple&aqs=chrome..69i57j69i60l3j69i65l2.1158j1j7&sourceid=chrome&ie=UTF-8",
@@ -50,8 +50,8 @@ var demo = new Vue({
 		"image": "dog.jpg"
 	},
 	{
-		"term": "Drupal",
-		"definition": "a hard round fruit that is white inside and has a smooth green, yellow, or red skin, which is called peel when it has been removed.",
+		"term": "gratitude",
+		"definition": "the quality of being thankful; readiness to show appreciation for and to return kindness.",
 		"data_community": "UBC IT",
 		"data_steward": "Alice Kim",
 		"definition_source": "https://www.google.ca/search?q=apple&oq=apple&aqs=chrome..69i57j69i60l3j69i65l2.1158j1j7&sourceid=chrome&ie=UTF-8",
@@ -64,8 +64,8 @@ var demo = new Vue({
 		"image": "dog.jpg"
 	},
 	{
-		"term": "Europe",
-		"definition": "a hard round fruit that is white inside and has a smooth green, yellow, or red skin, which is called peel when it has been removed.",
+		"term": "pie",
+		"definition": "a baked dish of fruit, or meat and vegetables, typically with a top and base of pastry.",
 		"data_community": "UBC IT",
 		"data_steward": "Alice Kim",
 		"definition_source": "https://www.google.ca/search?q=apple&oq=apple&aqs=chrome..69i57j69i60l3j69i65l2.1158j1j7&sourceid=chrome&ie=UTF-8",
@@ -78,8 +78,8 @@ var demo = new Vue({
 		"image": "dog.jpg"
 	},
 	{
-		"term": "Faculty",
-		"definition": "a hard round fruit that is white inside and has a smooth green, yellow, or red skin, which is called peel when it has been removed.",
+		"term": "flower",
+		"definition": "the seed-bearing part of a plant, consisting of reproductive organs (stamens and carpels) that are typically surrounded by a brightly colored corolla (petals) and a green calyx (sepals).",
 		"data_community": "UBC IT",
 		"data_steward": "Alice Kim",
 		"definition_source": "https://www.google.ca/search?q=apple&oq=apple&aqs=chrome..69i57j69i60l3j69i65l2.1158j1j7&sourceid=chrome&ie=UTF-8",
@@ -92,8 +92,8 @@ var demo = new Vue({
 		"image": "dog.jpg"
 	},
 	{
-		"term": "Great",
-		"definition": "a hard round fruit that is white inside and has a smooth green, yellow, or red skin, which is called peel when it has been removed.",
+		"term": "generosity",
+		"definition": "the quality of being kind and generous.",
 		"data_community": "UBC IT",
 		"data_steward": "Alice Kim",
 		"definition_source": "https://www.google.ca/search?q=apple&oq=apple&aqs=chrome..69i57j69i60l3j69i65l2.1158j1j7&sourceid=chrome&ie=UTF-8",
@@ -117,7 +117,9 @@ var demo = new Vue({
     computed: {
         // A computed property that holds only those articles that match the searchString.
         filteredArticles: function () {
-            var articles_array = this.articles,
+            var articles_array = this.articles.sort(function(a, b) {
+								return a.term - b.term;
+						}),
                 searchString = this.searchString;
 
             if(!searchString){
@@ -127,16 +129,62 @@ var demo = new Vue({
             searchString = searchString.trim().toLowerCase();
 
             articles_array = articles_array.filter(function(item){
-                if(item.term.toLowerCase().indexOf(searchString) === 0){
+            		var termToLowercase = item.term.toLowerCase();
+            		var defnToLowercase = item.definition.toLowerCase();
+								var indexOfTerm = item.term.toLowerCase().indexOf(searchString);
+            		var indexOfDefn = item.definition.toLowerCase().indexOf(searchString);
+            		
+                if(indexOfTerm === 0) {
+                		//item.term = '<span>' + item.term.substr(indexOfTerm, searchString.length) + '</span>' + item.term.substr(searchString.length, termToLowercase.length - searchString.length);
                 		item.highlighted = item.definition;
                     return item;
-                } else if (item.definition.toLowerCase().indexOf(searchString) !== -1) {
-										var index = item.definition.toLowerCase().indexOf(searchString);
-                		item.highlighted = "..." + item.definition.toLowerCase().substr(index, 5 + searchString.length + 5) + "...";
+                } else if (indexOfDefn !== -1 && searchString.indexOf(' ') === -1) {
+                		console.log(searchString + ' at ' + indexOfDefn);
+                		var w = item.definition.split(' ');
+                		var i = w.findIndex(function(word) {
+                				return word.indexOf(searchString) !== -1;
+                		});
+                		
+                		var start = i < 3 ? 0 : i - 3;
+                		var end   = w.length - i < 3 ? w.length : i + 3;
+                		var trailingStart = start === 0 ? '' : '...';
+                		var trailingEnd   = end   === w.length - 1 ? '' : '...';
+                		
+                		item.highlighted = trailingStart + w.slice(start,i).join(' ') + ' ' + w[i].substr(0, w[i].indexOf(searchString)) + '<span>' + w[i].substr(w[i].indexOf(searchString), searchString.length) + '</span>' + w[i].substr(w[i].indexOf(searchString) + searchString.length, w[i].length) + ' ' + w.slice(i + 1,end).join(' ') + trailingEnd;
+                		return item;
+                } else if (indexOfDefn !== -1) {
+                		var w = item.definition.split(' ');
+                		var startWord = w.findIndex(function(word, index) {
+                				return w.slice(0, index + 1).join(' ').length - 1 >= indexOfDefn;
+                		});
+                		var endWord = w.findIndex(function(word, index) {
+                				return w.slice(0, index + 1).join(' ').length - 1 >= indexOfDefn + searchString.length;
+                		});
+                		
+                		console.log(startWord)
+                		console.log(endWord)
+                		
+                		var start = startWord < 3 ? 0 : startWord - 3;
+                		var end   = w.length - endWord < 3 ? w.length : endWord + 3;
+                		var trailingStart = start === 0 ? '' : '...';
+                		var trailingEnd   = end   === w.length - 1 ? '' : '...';
+
+                		var prePhrase = w[startWord].substr(0, w[startWord].indexOf(searchString.split(' ')[0]));
+                		var phrase = searchString;
+                		console.log(">> " + w[endWord].indexOf(searchString.split(' ')[searchString.split(' ').length - 1]));
+
+                		var indexOfPostPhrase = w[endWord].indexOf(searchString.split(' ')[searchString.split(' ').length - 1]);
+                		
+                		var postPhrase = indexOfPostPhrase === -1 ? ' ' + w[endWord] : w[endWord].substr(indexOfPostPhrase + searchString.split(' ')[searchString.split(' ').length - 1].length, w[endWord].length);
+                		
+                		console.log(prePhrase)
+                		console.log(phrase)
+                		console.log(postPhrase)
+                		                		
+                		item.highlighted = trailingStart + w.slice(start, startWord).join(' ') + ' ' + prePhrase + '<span>' + phrase + '</span>' + postPhrase + ' ' + w.slice(endWord + 1, end).join(' ') + trailingEnd;
                 		return item;
                 }
             })
-
             // Return an array with the filtered data.
             return articles_array;
         }
