1ad052c9a49752075a2cedd389d6fc733f12d8a3
alice-0-kim <aliceykim0828@gmail.com>
2018-06-25 11:19:03 -0700

    covers 1) uppercase search string, and 2) uppercase letter included in the defn

--- typeahead.js
+++ typeahead.js
@@ -8,100 +8,66 @@ var demo = new Vue({
 
         articles: [
 	{
-		"term": "apple",
-		"definition": "a hard round fruit that is white inside and has a smooth green, yellow, or red skin, which is called peel when it has been removed.",
-		"data_community": "UBC IT",
-		"data_steward": "Alice Kim",
+		"term": "abstraction",
+		"definition": "A simplified representation of something more complex.",
 		"definition_source": "https://www.google.ca/search?q=apple&oq=apple&aqs=chrome..69i57j69i60l3j69i65l2.1158j1j7&sourceid=chrome&ie=UTF-8",
-		"status": "active",
-		"sensitivity": "true",
 		"example": "Apples are red.",
-		"acronym": "-",
-		"synonym": "-",
 		"highlighted": "",
 		"image": "dog.jpg"
 	},
 	{
-		"term": "dictionary",
-		"definition": "a book or electronic resource that lists the words of a language (typically in alphabetical order) and gives their meaning, or gives the equivalent words in a different language, often also providing information about pronunciation, origin, and usage.",
-		"data_community": "UBC IT",
-		"data_steward": "Alice Kim",
+		"term": "binary",
+		"definition": "A way of representing information using only two options.",
 		"definition_source": "https://www.google.ca/search?q=apple&oq=apple&aqs=chrome..69i57j69i60l3j69i65l2.1158j1j7&sourceid=chrome&ie=UTF-8",
-		"status": "active",
-		"sensitivity": "true",
 		"example": "Apples are red.",
-		"acronym": "-",
-		"synonym": "-",
 		"highlighted": "",
 		"image": "dog.jpg"
 	},
 	{
-		"term": "bliss",
-		"definition": "perfect happiness; great joy.",
-		"data_community": "UBC IT",
-		"data_steward": "Alice Kim",
+		"term": "byte",
+		"definition": "The most common fundamental unit of digital data eg. Kilobyte, Megabyte, etc. A single byte is 8 bits-worth of data.",
 		"definition_source": "https://www.google.ca/search?q=apple&oq=apple&aqs=chrome..69i57j69i60l3j69i65l2.1158j1j7&sourceid=chrome&ie=UTF-8",
-		"status": "active",
-		"sensitivity": "true",
 		"example": "Apples are red.",
-		"acronym": "-",
-		"synonym": "-",
 		"highlighted": "",
 		"image": "dog.jpg"
 	},
 	{
-		"term": "gratitude",
-		"definition": "the quality of being thankful; readiness to show appreciation for and to return kindness.",
-		"data_community": "UBC IT",
-		"data_steward": "Alice Kim",
+		"term": "domain name service",
+		"definition": "DNS; The service that translates URLs to IP addresses.",
 		"definition_source": "https://www.google.ca/search?q=apple&oq=apple&aqs=chrome..69i57j69i60l3j69i65l2.1158j1j7&sourceid=chrome&ie=UTF-8",
-		"status": "active",
-		"sensitivity": "true",
 		"example": "Apples are red.",
-		"acronym": "-",
-		"synonym": "-",
 		"highlighted": "",
 		"image": "dog.jpg"
 	},
 	{
-		"term": "pie",
-		"definition": "a baked dish of fruit, or meat and vegetables, typically with a top and base of pastry.",
-		"data_community": "UBC IT",
-		"data_steward": "Alice Kim",
+		"term": "internet",
+		"definition": "A group of computers and servers that are connected to each other.",
 		"definition_source": "https://www.google.ca/search?q=apple&oq=apple&aqs=chrome..69i57j69i60l3j69i65l2.1158j1j7&sourceid=chrome&ie=UTF-8",
-		"status": "active",
-		"sensitivity": "true",
 		"example": "Apples are red.",
-		"acronym": "-",
-		"synonym": "-",
-		"highlighted": "",	
-		"image": "dog.jpg"
-	},
-	{
-		"term": "flower",
-		"definition": "the seed-bearing part of a plant, consisting of reproductive organs (stamens and carpels) that are typically surrounded by a brightly colored corolla (petals) and a green calyx (sepals).",
-		"data_community": "UBC IT",
-		"data_steward": "Alice Kim",
-		"definition_source": "https://www.google.ca/search?q=apple&oq=apple&aqs=chrome..69i57j69i60l3j69i65l2.1158j1j7&sourceid=chrome&ie=UTF-8",
-		"status": "active",
-		"sensitivity": "true",
-		"example": "Apples are red.",
-		"acronym": "-",
-		"synonym": "-",
 		"highlighted": "",
 		"image": "dog.jpg"
 	},
 	{
-		"term": "generosity",
-		"definition": "the quality of being kind and generous.",
-		"data_community": "UBC IT",
-		"data_steward": "Alice Kim",
+		"term": "pixel",
+		"definition": "Short for \"picture element\", the fundamental unit of a digital image, typically a tiny square or dot that contains a single point of color of a larger image.",
 		"definition_source": "https://www.google.ca/search?q=apple&oq=apple&aqs=chrome..69i57j69i60l3j69i65l2.1158j1j7&sourceid=chrome&ie=UTF-8",
-		"status": "active",
-		"sensitivity": "true",
 		"example": "Apples are red.",
-		"acronym": "-",
-		"synonym": "-",
+		"highlighted": "",
+		"image": "dog.jpg"
+	},
+	{
+		"term": "variable",
+		"definition": "A placeholder for a piece of information that can change.",
+		"definition_source": "https://www.google.ca/search?q=apple&oq=apple&aqs=chrome..69i57j69i60l3j69i65l2.1158j1j7&sourceid=chrome&ie=UTF-8",
+		"example": "Apples are red.",
+		"highlighted": "",
+		"image": "dog.jpg"
+	},
+	{
+		"term": "website",
+		"definition": "A collection of interlinked web pages on the World Wide Web.",
+		"definition_source": "https://www.google.ca/search?q=apple&oq=apple&aqs=chrome..69i57j69i60l3j69i65l2.1158j1j7&sourceid=chrome&ie=UTF-8",
+		"example": "Apples are red.",
 		"highlighted": "",
 		"image": "dog.jpg"
 	}]
@@ -118,7 +84,9 @@ var demo = new Vue({
         // A computed property that holds only those articles that match the searchString.
         filteredArticles: function () {
             var articles_array = this.articles.sort(function(a, b) {
-								return a.term - b.term;
+								if(a.term < b.term) return -1;
+								if(a.term > b.term) return 1;
+								return 0;
 						}),
                 searchString = this.searchString;
 
@@ -142,15 +110,15 @@ var demo = new Vue({
                 		console.log(searchString + ' at ' + indexOfDefn);
                 		var w = item.definition.split(' ');
                 		var i = w.findIndex(function(word) {
-                				return word.indexOf(searchString) !== -1;
+                				return word.toLowerCase().indexOf(searchString) !== -1;
                 		});
                 		
                 		var start = i < 3 ? 0 : i - 3;
                 		var end   = w.length - i < 3 ? w.length : i + 3;
                 		var trailingStart = start === 0 ? '' : '...';
                 		var trailingEnd   = end   === w.length - 1 ? '' : '...';
-                		
-                		item.highlighted = trailingStart + w.slice(start,i).join(' ') + ' ' + w[i].substr(0, w[i].indexOf(searchString)) + '<span>' + w[i].substr(w[i].indexOf(searchString), searchString.length) + '</span>' + w[i].substr(w[i].indexOf(searchString) + searchString.length, w[i].length) + ' ' + w.slice(i + 1,end).join(' ') + trailingEnd;
+
+                		item.highlighted = trailingStart + w.slice(start,i).join(' ') + ' ' + w[i].substr(0, w[i].toLowerCase().indexOf(searchString)) + '<span>' + w[i].substr(w[i].toLowerCase().indexOf(searchString), searchString.length) + '</span>' + w[i].substr(w[i].toLowerCase().indexOf(searchString) + searchString.length, w[i].length) + ' ' + w.slice(i + 1,end).join(' ') + trailingEnd;
                 		return item;
                 } else if (indexOfDefn !== -1) {
                 		var w = item.definition.split(' ');
@@ -169,11 +137,11 @@ var demo = new Vue({
                 		var trailingStart = start === 0 ? '' : '...';
                 		var trailingEnd   = end   === w.length - 1 ? '' : '...';
 
-                		var prePhrase = w[startWord].substr(0, w[startWord].indexOf(searchString.split(' ')[0]));
+                		var prePhrase = w[startWord].substr(0, w[startWord].toLowerCase().indexOf(searchString.split(' ')[0]));
                 		var phrase = searchString;
-                		console.log(">> " + w[endWord].indexOf(searchString.split(' ')[searchString.split(' ').length - 1]));
+                		console.log(">> " + w[endWord].toLowerCase().indexOf(searchString.split(' ')[searchString.split(' ').length - 1]));
 
-                		var indexOfPostPhrase = w[endWord].indexOf(searchString.split(' ')[searchString.split(' ').length - 1]);
+                		var indexOfPostPhrase = w[endWord].toLowerCase().indexOf(searchString.split(' ')[searchString.split(' ').length - 1]);
                 		
                 		var postPhrase = indexOfPostPhrase === -1 ? ' ' + w[endWord] : w[endWord].substr(indexOfPostPhrase + searchString.split(' ')[searchString.split(' ').length - 1].length, w[endWord].length);
                 		
@@ -186,6 +154,7 @@ var demo = new Vue({
                 }
             })
             // Return an array with the filtered data.
+            //console.log(JSON.stringify(articles_array))
             return articles_array;
         }
     }
